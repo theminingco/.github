643ef98d618c711f263b7d07448d6ff8b73f47c1
Wilhelm Thieme <wjthieme@gmail.com>
2023-02-28 16:57:38 +0100

    Added simple FastAPI server to api package

--- core/parser.py
+++ core/parser.py
@@ -3,6 +3,6 @@ from sys import stderr, exit
 
 class Parser(ArgumentParser):
     def error(self, message):
-        stderr.write('error: %s\n' % message)
+        stderr.write("error: %s\n" % message)
         self.print_help()
         exit(2)
