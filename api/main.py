8f19e9a4902dfa3c6b055056af0f56fb035cfe2a
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-28 16:58:42 +0100

    Added simple FastAPI server to api package (#4)

--- api/main.py
+++ api/main.py
@@ -1,3 +1,24 @@
+from fastapi import FastAPI
+from uvicorn import run
+
+app = FastAPI(
+    title="jewl.app",
+    version="1",
+    docs_url=None,
+    redoc_url="/"
+)
+
+@app.post("/infer")
+def run_inference():
+    return {"Hello": "World"}
 
 def main(args):
-    print(args)
+    log_level = "trace" if args.verbose else "info"
+    port = args.port if hasattr(args, "port") else 2000
+    run(
+        "api.main:app",
+        host="0.0.0.0",
+        port=port,
+        log_level=log_level,
+        reload=args.watch
+    )
