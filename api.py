5acaae3c76b37b24883a1fc827d6cd4926243050
Wilhelm Thieme <wjthieme@gmail.com>
2023-03-01 11:53:37 +0100

    Made redoc page consistent

--- api.py
+++ api.py
@@ -1,26 +1,37 @@
 """This module contains all the code related to running an inference api."""
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html
 from uvicorn import run
 
 app = FastAPI(
-    title="jewl.app",
-    version="1",
+    title="jewl-ai",
+    version="docs",
     docs_url=None,
-    redoc_url="/"
+    redoc_url=None
 )
 
-@app.post("/infer")
+@app.get("/", include_in_schema=False)
+def redoc():
+    """Return the redoc documentation page."""
+    return get_redoc_html(
+        openapi_url="/openapi.json",
+        title="jewl.app",
+        redoc_favicon_url="https://jewl.app/favicon-32x32.png"
+    )
+
+@app.post("/v1/infer")
 def run_inference():
-    """Run inference on a sample using a trained model"""
+    """Run inference on a sample using a trained model."""
     return {"Hello": "World"}
 
 
 def main(args):
     """The entrypoint of the api module."""
     log_level = "trace" if args.verbose else "info"
-    port = args.port if hasattr(args, "port") else 2000
+    port = 2000 if args.port is None else args.port
+    print(port)
     run(
-        "api.main:app",
+        "api:app",
         host="0.0.0.0",
         port=port,
         log_level=log_level,
