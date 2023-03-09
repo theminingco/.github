c396fdbdc6e19e673d2dcc3c4f7449585b272373
Will <82029448+wjthieme@users.noreply.github.com>
2023-03-09 22:23:56 +0100

    Made some improvements to the transformer model (#28)
    * Made some improvements to the transformer model
    
    * Fixed failing tests

--- perceptron/create.py
+++ perceptron/create.py
@@ -6,24 +6,21 @@ from math import log
 from torch import tensor, Tensor, load, save
 from torch import arange, exp, sin, cos, equal
 from torch import round as round_tensor
-from torch import ones, triu, zeros_like, rand, zeros
+from torch import sum as sum_dim
+from torch import ones, tril, ones_like, rand, zeros
 from torch.nn import Module, Dropout
 from torch.nn import TransformerDecoder, TransformerDecoderLayer
 from torch.nn.init import xavier_normal_
+from torch.nn.functional import pad
 
 class Transformer(Module):
-    """A simple Transformer Decoder model."""
+    """A full Transformer Decoder model."""
 
     def __init__(self, nfeatures: int, nhid: int, nhead: int, nlayers: int, dropout: float) -> None:
         super().__init__()
-
+        self.padding = Padding(nfeatures)
         self.positional = Positional(nfeatures, dropout)
-        decoder_layer = TransformerDecoderLayer(nfeatures, nhead, nhid, dropout)
-        self.decoder = TransformerDecoder(decoder_layer, nlayers)
-
-        for p in self.parameters():
-            if p.dim() > 1:
-                xavier_normal_(p)
+        self.decoder = Decoder(nfeatures, nhid, nhead, nlayers, dropout)
 
     @classmethod
     def create(cls, nfeatures=256, nhid=8, nhead=8, nlayers=2, dropout=0.1, **_):
@@ -47,17 +44,63 @@ class Transformer(Module):
         }
         save(spec, path)
 
+    def forward(self, x: Tensor) -> Tensor:
+        """Propagate through the model."""
+        x = self.padding(x)
+        x = self.positional(x)
+        return self.decoder(x)
+
+
+class Decoder(Module):
+    """A transformer decoder module."""
+
+    def __init__(self, nfeatures: int, nhid: int, nhead: int, nlayers: int, dropout: float) -> None:
+        super().__init__()
+        decoder_layer = TransformerDecoderLayer(nfeatures, nhead, nhid, dropout)
+        self.decoder = TransformerDecoder(decoder_layer, nlayers)
+
+        for p in self.parameters():
+            if p.dim() > 1:
+                xavier_normal_(p)
+
+    def _mem(self, x: Tensor) -> Tensor:
+        """Create an empty memory tensor."""
+        return ones_like(x)
+
+    def _msk(self, x: Tensor) -> Tensor:
+        """Create a padding mask for tensor x."""
+        msk = sum_dim(x, dim=-1)
+        return (msk == 0).transpose(0, 1)
+
     def _nopeek(self, x: Tensor) -> Tensor:
-        nopeek = x.size(0)
-        nopeek = ones(nopeek, nopeek)
-        nopeek = triu(nopeek).transpose(0, 1)
-        return nopeek.masked_fill(nopeek == 0, float('-inf')).masked_fill(nopeek == 1, float(0.0))
+        """Create a nopeek mask for tensor x."""
+        size = x.size(0)
+        nopeek = tril(ones(size, size, device=x.device))
+        nopeek = nopeek.masked_fill(nopeek == 0, float('-inf'))
+        return nopeek.masked_fill(nopeek == 1, float(0.0))
 
     def forward(self, x: Tensor) -> Tensor:
         """Propagate through the model."""
-        nopeek_msk = self._nopeek(x)
-        msk = zeros_like(x)
-        return self.decoder(x, msk, tgt_mask=nopeek_msk)
+        return self.decoder(
+            x,
+            self._mem(x),
+            tgt_mask=self._nopeek(x),
+            tgt_key_padding_mask=self._msk(x)
+        )
+
+
+class Padding(Module):
+    """Pad the last dimension of a tensor to a specific length."""
+
+    def __init__(self, nfeatures: int) -> None:
+        super().__init__()
+        self.nfeatures = nfeatures
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Propagate through the model."""
+        pad_size = self.nfeatures - x.size(-1)
+        return pad(x, (0, pad_size))
+
 
 class Positional(Module):
     """Encode positional information into an a tensor."""
@@ -98,7 +141,7 @@ if __name__ == "__main__":
         print(f"Saved {args.name} model to {args.path}")
 
     model.eval()
-    model_input = rand(32, 32, args.nfeatures)
+    model_input = rand(16, 32, args.nfeatures)
     model_output = model(model_input)
     print(f"{model_input.size()} -> {model_output.size()}")
 
@@ -111,6 +154,27 @@ def test_model_forward() -> None:
     test_input = rand(16, 32, 8)
     test_output = test_model(test_input)
     assert test_input.size() == test_output.size(), f"{test_input.size()} != {test_output.size()}"
+
+def test_padding_small() -> None:
+    """Test whether padding is done for a tensor with not enough features."""
+    test_model = Padding(8)
+    test_input = zeros(32, 32, 7)
+    test_output_size = list(test_model(test_input).size())
+    assert test_output_size == [32, 32, 8], f"{test_output_size} != {[32, 32, 8]}"
+
+def test_padding_exact() -> None:
+    """Test whether padding is done for a tensor with exactly enough features."""
+    test_model = Padding(8)
+    test_input = zeros(32, 32, 8)
+    test_output_size = list(test_model(test_input).size())
+    assert test_output_size == [32, 32, 8], f"{test_output_size} != {[32, 32, 8]}"
+
+def test_padding_big() -> None:
+    """Test whether padding is done for a tensor with too many features."""
+    test_model = Padding(8)
+    test_input = zeros(32, 32, 9)
+    test_output_size = list(test_model(test_input).size())
+    assert test_output_size == [32, 32, 8], f"{test_output_size} != {[32, 32, 8]}"
 
 def test_positional_layer() -> None:
     """Test whether positional embedding is generated properly."""
