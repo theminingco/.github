5bee91f6081eb9712be72fe3bdc73b361ca4fe38
Wilhelm Thieme <wjthieme@gmail.com>
2023-03-11 19:20:32 +0100

    Added high and low prices to sticks

--- data/sticks.py
+++ data/sticks.py
@@ -1,22 +1,37 @@
 """This module contains all the code related to getting all available symbols."""
 from argparse import ArgumentParser
+from typing import List
 from binance.spot import Spot
-from util.stick import print_sticks
+from util.stick import Stick, print_sticks
 
 client = Spot()
 
-def get_candle_sticks(symbol: str, interval: str, limit: int, **_) -> None:
+def get_candle_sticks(symbol: str, interval: str, limit: int, **_) -> List[Stick]:
     """The entrypoint of the sticks module."""
     klines = client.klines(symbol, interval, limit=limit)
-    return klines
+    sticks = []
+    for kline in klines:
+        stick = Stick(
+            float(kline[1]),
+            float(kline[2]),
+            float(kline[3]),
+            float(kline[4]),
+            float(kline[5]),
+            float(kline[7]),
+            float(kline[8]),
+            float(kline[9]),
+            float(kline[10])
+        )
+        sticks.append(stick)
+    return sticks
 
 if __name__ == "__main__":
+    interval_choices = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
     parser = ArgumentParser()
     parser.add_argument("-s", "--symbol", type=str, default="BTCUSDT")
-    parser.add_argument("-i", "--interval", type=str, choices=[""], default="15m")
+    parser.add_argument("-i", "--interval", type=str, choices=interval_choices, default="15m")
     parser.add_argument("-n", "--limit", type=int, default=10)
     args = parser.parse_args()
 
-    sticks = get_candle_sticks(**vars(args))
-    sticks = [ (float(stick[1]), float(stick[4])) for stick in sticks ]
-    print_sticks(sticks)
+    candle_sticks = get_candle_sticks(**vars(args))
+    print_sticks(candle_sticks)
--- util/stick.py
+++ util/stick.py
@@ -1,45 +1,64 @@
 """This module contains all the code related to printing ascii box plots."""
 from argparse import ArgumentParser
-from typing import List, Tuple
+from dataclasses import dataclass
+from os import popen
+from typing import List
 from random import randint
+from math import floor, log10
 from numpy import amin, amax
 
-def print_legend(minimum: float, maximum: float, length=50):
+@dataclass
+class Stick:
+    """A dataclass for a Kline candlestick."""
+    open_price: float
+    high_price: float
+    low_price: float
+    close_price: float
+    volume: float
+    quote_volume: float
+    num_trades: int
+    taker_base_volume: float
+    taker_quote_volume: float
+
+def print_legend(minimum: float, maximum: float, length: int):
     """Print a legend for candlesitcks."""
+    def _round_sig(x: float):
+        return round(x, 3 - int(floor(log10(abs(x)))) - 1)
     length = int(length / 2) * 2
-    middle = (minimum + maximum) / 2
-    legend = f"{minimum}"
-    legend = legend + " " * int(length / 2 - len(legend))
-    legend = legend + f"{middle}"
-    legend = legend + " " * (length - len(legend))
-    legend = legend + f"{maximum}"
-    print(legend)
+    middle = round((minimum + maximum) / 2, 2)
+    low_price = f"{_round_sig(minimum)}"
+    mid_price = f"{_round_sig(middle)}"
+    high_price =  f"{_round_sig(maximum)}"
+    space =  " " * int(length - len(low_price) - len(mid_price) - len(high_price))
+    space_splitter = int(len(space) / 2)
+    print(f"{low_price}{space[:space_splitter]}{mid_price}{space[space_splitter:]}{high_price}")
 
-def print_stick(open_price: float, close_price: float, step: float, offset: float):
+def print_stick(stick: Stick, step: float, offset: float):
     """Print a single candlestick."""
-    minimum = amin([open_price, close_price]) - offset
-    maximum = amax([open_price, close_price]) - offset
-    offset = int(minimum / step)
-    length = amax([int((maximum - minimum) / step), 1])
-    prefix = "\x1b[32m" if open_price < close_price else "\x1b[31m"
-    space = " " * offset
-    stick = "=" * length
+    stick_start = amin([stick.open_price, stick.close_price])
+    stick_end = amax([stick.open_price, stick.close_price])
+    prefix = "\x1b[32m" if stick.open_price < stick.close_price else "\x1b[31m"
+    space = " " * int((stick.low_price - offset) / step)
+    pre_stick = "-" * int((stick_start - stick.low_price) / step)
+    stick_space = "=" * int((stick_end - stick_start) / step)
+    post_stick = "-" * int((stick.high_price - stick_end) / step)
     suffix = "\x1b[0m"
+    print(f"{prefix}{space}{pre_stick}[{stick_space}]{post_stick}{suffix}")
 
-    print(f"{prefix}{space}{stick}{suffix}")
-
-def print_sticks(sticks: List[Tuple[float, float]], length=50, **_):
+def print_sticks(sticks: List[Stick], length=0, **_):
     """Print one or more candlesticks and an accompanying legend."""
-    minimum, maximum = amin(sticks), amax(sticks)
+    _, columns = popen("stty size", "r").read().split()
+    length = int(columns) if length is None or length == 0 else length
+    minimum = amin([ stick.low_price for stick in sticks ])
+    maximum = amax([ stick.high_price for stick in sticks ])
     print_legend(minimum, maximum, length)
     step = (maximum - minimum) / length
     for stick in sticks:
-        print_stick(stick[0], stick[1], step, minimum)
+        print_stick(stick, step, minimum)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-n", "--amount", default=10, type=int)
-    parser.add_argument("-l", "--length", default=50, type=int)
     args = parser.parse_args()
 
     data = []
@@ -47,7 +66,9 @@ if __name__ == "__main__":
     for n in range(args.amount):
         opening = previous + randint(-20, 20)
         closing = previous + randint(-200, 200)
-        data.append((opening, closing))
+        lowing = amin([opening, closing]) + randint(-20, -1)
+        highing = amax([opening, closing]) + randint(1, 20)
+        data.append(Stick(float(opening), float(highing), float(lowing), float(closing), 0, 0, 0, 0, 0))
         previous = closing
 
     print_sticks(data, **vars(args))
