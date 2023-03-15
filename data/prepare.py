6d160978be914cdd53581f5f1199914bb935f0c5
Wilhelm Thieme <wjthieme@gmail.com>
2023-03-15 11:36:15 +0100

    Typing

--- data/prepare.py
+++ data/prepare.py
@@ -8,25 +8,25 @@ from torch.nn.functional import pad, normalize
 class DataFrame(Dataset):
     """A class for loading minibatches in parallel."""
 
-    def __init__(self, path: str, batch_size: int = 256, shuffle: bool = True, device: str = "cpu"):
+    def __init__(self, path: str, batch_size: int = 256, shuffle: bool = True, device: str = "cpu") -> None:
         self.device = device
         self.files = glob(f"{path}/*.pt")
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.loader = iter(DataLoader(self, batch_size=batch_size, shuffle=shuffle))
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Get the amount of data in the set."""
         return len(self.files)
 
-    def __getitem__(self, index: int):
+    def __getitem__(self, index: int) -> Tensor:
         """Return a tensor for a given index."""
         file = self.files[index]
         sticks = load(file).to(self.device)
         sticks = normalize(sticks, dim=1)
         return pad(sticks, (0, 4))
 
-    def __next__(self):
+    def __next__(self) -> Tensor:
         """Get the next batch."""
         try:
             next_batch = next(self.loader)
