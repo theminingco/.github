eb17bfd00aec4fb49997a4789dcf86be42e67f6a
Will <82029448+wjthieme@users.noreply.github.com>
2024-02-14 11:33:45 -0500

    Add default wallets in case there are none detected (#120)
    * Remove gh actions deploy environment
    
    * Add default wallets in case there are none detected

--- web/hooks/solana.tsx
+++ web/hooks/solana.tsx
@@ -1,15 +1,24 @@
 import type { PropsWithChildren, ReactElement } from "react";
 import React, { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
 
 const SolanaProvider = (props: PropsWithChildren): ReactElement => {
   const endpoint = useMemo(() => {
     return "https://solana-mainnet.g.alchemy.com/v2/cEI9IVd9_paAuyKLsU_zN4UdM-ASnyhq";
   }, []);
 
+  const wallets = useMemo(() => {
+    return [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter()
+    ];
+  }, []);
+
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect>
         {props.children}
       </WalletProvider>
     </ConnectionProvider>
